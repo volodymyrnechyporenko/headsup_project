af8df462cb56835a8ec4e49a6faac7b110a84607
volodymyrnechyporenko <volodymyrnechyporenko@gmail.com>
2021-10-30 20:34:02 +0300

    Fixed issues

--- node_modules/webpack-dev-server/client/utils/reloadApp.js
+++ node_modules/webpack-dev-server/client/utils/reloadApp.js
@@ -8,21 +8,13 @@ function reloadApp(_ref, status) {
 
   if (status.isUnloading) {
     return;
-  } // TODO Workaround for webpack v4, `__webpack_hash__` is not replaced without HotModuleReplacement plugin
+  }
 
-
-  var webpackHash = // eslint-disable-next-line camelcase
-  typeof __webpack_hash__ !== "undefined" ? // eslint-disable-next-line camelcase
-  __webpack_hash__ : status.previousHash || "";
-  var isInitial = status.currentHash.indexOf(webpackHash) === 0;
+  var currentHash = status.currentHash,
+      previousHash = status.previousHash;
+  var isInitial = currentHash.indexOf(previousHash) >= 0;
 
   if (isInitial) {
-    var isLegacyInitial = webpackHash === "" && hot === false && liveReload === true;
-
-    if (isLegacyInitial) {
-      status.previousHash = status.currentHash;
-    }
-
     return;
   }
 
